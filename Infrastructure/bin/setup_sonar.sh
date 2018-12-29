92d519eb73ef4078399228b8fa38f1dc206a90d2
LekWut <ukkarapol@gmail.com>
2018-12-29 08:20:58 +0700

    setup by student

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -14,3 +14,4 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 # oc new-app -f ../templates/sonarqube.yaml --param .....
 
 # To be Implemented by Student
+oc new-app -f ../templates/sonarqube.yaml --param POSTGRES_PASSWORD=c29uYXI= --param POSTGRES_USERNAME=c29uYXI= --param POSTGRES_DATABASE_NAME=c29uYXI= --param POSTGRES_PERSISTENT_VOLUME_CLAIM_SIZE=4Gi --param SONARQUBE_JDBC_PASSWORD=sonar --param SONARQUBE_JDBC_USERNAME=sonar --param SONARQUBE_JDBC_URL="jdbc:postgresql://postgresql/sonar" --param SONARQUBE_PERSISTENT_VOLUME_SIZE=4Gi -n ${GUID}-sonarqube
